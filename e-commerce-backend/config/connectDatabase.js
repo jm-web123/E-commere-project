a80ced33baa5f10f435997ebd6b25f60369cdfff
jm-web123 <manjupriyaj95@gmail.com>
2025-07-12 14:47:04 +0530

    second commit

--- e-commerce-backend/config/connectDatabase.js
+++ e-commerce-backend/config/connectDatabase.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 const connectDatabase = () =>{
-    mongoose.connect("mongodb://localhost:27017/").then(() => {
+    mongoose.connect("mongodb+srv://manjupriyaj95:1992@cluster0.9phszst.mongodb.net/text?retryWrites=true&w=majority&appName=Cluster0").then(() => {
 console.log("MongoDB Connected..")
     })
 };
